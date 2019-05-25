0b4a65a86abf6b7c176187d1dafa3f1c31271a62
shelbaum <45436319+shelbaum@users.noreply.github.com>
2019-05-25 16:48:30 -0700

    Add files via upload

---
+++ load_elements.js
@@ -0,0 +1,574 @@
+var json = [{
+      "data" : {
+        "id" : "255",
+        "shared_name" : "Topic",
+        "EdgeCount" : 2,
+        "Outdegree" : 2,
+        "Indegree" : 0,
+        "name" : "Topic",
+        "SUID" : 255,
+        "type" : "Researcher-generated",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -151.33279418945312,
+        "y" : 37.06879009519304
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "246",
+        "shared_name" : "Presidential Document",
+        "EdgeCount" : 1,
+        "Outdegree" : 1,
+        "Indegree" : 0,
+        "name" : "Presidential Document",
+        "SUID" : 246,
+        "type" : "Executive",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -108.15211486816406,
+        "y" : 94.49325908933366
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "243",
+        "shared_name" : "Statutes at Large",
+        "EdgeCount" : 5,
+        "Outdegree" : 3,
+        "Indegree" : 2,
+        "name" : "Statutes at Large",
+        "SUID" : 243,
+        "type" : "Legislative",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 148.0873449361322,
+        "y" : -13.347911834716797
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "241",
+        "shared_name" : "Public Law",
+        "EdgeCount" : 7,
+        "Outdegree" : 4,
+        "Indegree" : 3,
+        "name" : "Public Law",
+        "SUID" : 241,
+        "type" : "Legislative",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 69.67431640625,
+        "y" : 85.40991558347429
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "240",
+        "shared_name" : "Popular Name",
+        "EdgeCount" : 3,
+        "Outdegree" : 3,
+        "Indegree" : 0,
+        "name" : "Popular Name",
+        "SUID" : 240,
+        "type" : "Legislative",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 11.87188720703125,
+        "y" : 146.45334209714616
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "236",
+        "shared_name" : "Regulation Docket",
+        "EdgeCount" : 2,
+        "Outdegree" : 0,
+        "Indegree" : 2,
+        "name" : "Regulation Docket",
+        "SUID" : 236,
+        "type" : "Executive",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 45.66510009765625,
+        "y" : -259.90790496553694
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "232",
+        "shared_name" : "U.S.C. Section",
+        "EdgeCount" : 7,
+        "Outdegree" : 2,
+        "Indegree" : 5,
+        "name" : "U.S.C. Section",
+        "SUID" : 232,
+        "type" : "Legislative",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -5.92616997480348,
+        "y" : 42.01123046875
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "228",
+        "shared_name" : "Congressional Record",
+        "EdgeCount" : 1,
+        "Outdegree" : 0,
+        "Indegree" : 1,
+        "name" : "Congressional Record",
+        "SUID" : 228,
+        "type" : "Legislative",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 261.18438720703125,
+        "y" : 231.3685337475368
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "227",
+        "shared_name" : "Bill Number",
+        "EdgeCount" : 2,
+        "Outdegree" : 1,
+        "Indegree" : 1,
+        "name" : "Bill Number",
+        "SUID" : 227,
+        "type" : "Legislative",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 172.4378662109375,
+        "y" : 158.08661236081804
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "225",
+        "shared_name" : "FR NPRM",
+        "EdgeCount" : 4,
+        "Outdegree" : 2,
+        "Indegree" : 2,
+        "name" : "FR NPRM",
+        "SUID" : 225,
+        "type" : "Executive",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -59.064666748046875,
+        "y" : -232.97534595217024
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "223",
+        "shared_name" : "FR Final Rule",
+        "EdgeCount" : 5,
+        "Outdegree" : 3,
+        "Indegree" : 2,
+        "name" : "FR Final Rule",
+        "SUID" : 223,
+        "type" : "Executive",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 12.638153076171875,
+        "y" : -148.3353236743382
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "221",
+        "shared_name" : "C.F.R. Section",
+        "EdgeCount" : 9,
+        "Outdegree" : 3,
+        "Indegree" : 6,
+        "name" : "C.F.R. Section",
+        "SUID" : 221,
+        "type" : "Executive",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -11.05364990234375,
+        "y" : -37.77178936362244
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "220",
+        "shared_name" : "Agency",
+        "EdgeCount" : 6,
+        "Outdegree" : 3,
+        "Indegree" : 3,
+        "name" : "Agency",
+        "SUID" : 220,
+        "type" : "Executive",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -86.05998229980469,
+        "y" : -130.3105128833226
+      },
+      "selected" : false
+    },
+    {
+      "data" : {
+        "id" : "257",
+        "source" : "255",
+        "target" : "232",
+        "shared_name" : "Topic (U.S.C. Index) U.S.C. Section",
+        "name" : "Topic (U.S.C. Index) U.S.C. Section",
+        "interaction" : "U.S.C. Index",
+        "SUID" : 257,
+        "shared_interaction" : "U.S.C. Index",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "256",
+        "source" : "255",
+        "target" : "221",
+        "shared_name" : "Topic (C.F.R. Index) C.F.R. Section",
+        "name" : "Topic (C.F.R. Index) C.F.R. Section",
+        "interaction" : "C.F.R. Index",
+        "SUID" : 256,
+        "shared_interaction" : "C.F.R. Index",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "247",
+        "source" : "246",
+        "target" : "221",
+        "shared_name" : "Presidential Document (Parallel Table of Authorities and Rules) C.F.R. Section",
+        "name" : "Presidential Document (Parallel Table of Authorities and Rules) C.F.R. Section",
+        "interaction" : "Parallel Table of Authorities and Rules",
+        "SUID" : 247,
+        "shared_interaction" : "Parallel Table of Authorities and Rules",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "254",
+        "source" : "243",
+        "target" : "232",
+        "shared_name" : "Statutes at Large (Table III or Classification Tables) U.S.C. Section",
+        "name" : "Statutes at Large (Table III or Classification Tables) U.S.C. Section",
+        "interaction" : "Table III or Classification Tables",
+        "SUID" : 254,
+        "shared_interaction" : "Table III or Classification Tables",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "253",
+        "source" : "243",
+        "target" : "241",
+        "shared_name" : "Statutes at Large (PL number printed in Statutes at Large) Public Law",
+        "name" : "Statutes at Large (PL number printed in Statutes at Large) Public Law",
+        "interaction" : "PL number printed in Statutes at Large",
+        "SUID" : 253,
+        "shared_interaction" : "PL number printed in Statutes at Large",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "252",
+        "source" : "243",
+        "target" : "221",
+        "shared_name" : "Statutes at Large (Parallel Table of Authorities and Rules) C.F.R. Section",
+        "name" : "Statutes at Large (Parallel Table of Authorities and Rules) C.F.R. Section",
+        "interaction" : "Parallel Table of Authorities and Rules",
+        "SUID" : 252,
+        "shared_interaction" : "Parallel Table of Authorities and Rules",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "251",
+        "source" : "241",
+        "target" : "232",
+        "shared_name" : "Public Law (Table III or Classification Tables) U.S.C. Section",
+        "name" : "Public Law (Table III or Classification Tables) U.S.C. Section",
+        "interaction" : "Table III or Classification Tables",
+        "SUID" : 251,
+        "shared_interaction" : "Table III or Classification Tables",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "250",
+        "source" : "241",
+        "target" : "243",
+        "shared_name" : "Public Law (Browse Statutes at Large) Statutes at Large",
+        "name" : "Public Law (Browse Statutes at Large) Statutes at Large",
+        "interaction" : "Browse Statutes at Large",
+        "SUID" : 250,
+        "shared_interaction" : "Browse Statutes at Large",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "249",
+        "source" : "241",
+        "target" : "221",
+        "shared_name" : "Public Law (Parallel Table of Authorities and Rules) C.F.R. Section",
+        "name" : "Public Law (Parallel Table of Authorities and Rules) C.F.R. Section",
+        "interaction" : "Parallel Table of Authorities and Rules",
+        "SUID" : 249,
+        "shared_interaction" : "Parallel Table of Authorities and Rules",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "248",
+        "source" : "241",
+        "target" : "227",
+        "shared_name" : "Public Law (Bill number printed in margin) Bill Number",
+        "name" : "Public Law (Bill number printed in margin) Bill Number",
+        "interaction" : "Bill number printed in margin",
+        "SUID" : 248,
+        "shared_interaction" : "Bill number printed in margin",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "245",
+        "source" : "240",
+        "target" : "232",
+        "shared_name" : "Popular Name (Popular Names Table) U.S.C. Section",
+        "name" : "Popular Name (Popular Names Table) U.S.C. Section",
+        "interaction" : "Popular Names Table",
+        "SUID" : 245,
+        "shared_interaction" : "Popular Names Table",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "244",
+        "source" : "240",
+        "target" : "243",
+        "shared_name" : "Popular Name (Popular Names Table) Statutes at Large",
+        "name" : "Popular Name (Popular Names Table) Statutes at Large",
+        "interaction" : "Popular Names Table",
+        "SUID" : 244,
+        "shared_interaction" : "Popular Names Table",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "242",
+        "source" : "240",
+        "target" : "241",
+        "shared_name" : "Popular Name (Popular Names Table) Public Law",
+        "name" : "Popular Name (Popular Names Table) Public Law",
+        "interaction" : "Popular Names Table",
+        "SUID" : 242,
+        "shared_interaction" : "Popular Names Table",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "259",
+        "source" : "232",
+        "target" : "241",
+        "shared_name" : "U.S.C. Section (Source citation) Public Law",
+        "name" : "U.S.C. Section (Source citation) Public Law",
+        "interaction" : "Source citation",
+        "SUID" : 259,
+        "shared_interaction" : "Source citation",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "258",
+        "source" : "232",
+        "target" : "221",
+        "shared_name" : "U.S.C. Section (Parallel Table of Authorities and Rules) C.F.R. Section",
+        "name" : "U.S.C. Section (Parallel Table of Authorities and Rules) C.F.R. Section",
+        "interaction" : "Parallel Table of Authorities and Rules",
+        "SUID" : 258,
+        "shared_interaction" : "Parallel Table of Authorities and Rules",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "229",
+        "source" : "227",
+        "target" : "228",
+        "shared_name" : "Bill Number (Bill history) Congressional Record",
+        "name" : "Bill Number (Bill history) Congressional Record",
+        "interaction" : "Bill history",
+        "SUID" : 229,
+        "shared_interaction" : "Bill history",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "239",
+        "source" : "225",
+        "target" : "236",
+        "shared_name" : "FR NPRM (Regulations.gov) Regulation Docket",
+        "name" : "FR NPRM (Regulations.gov) Regulation Docket",
+        "interaction" : "Regulations.gov",
+        "SUID" : 239,
+        "shared_interaction" : "Regulations.gov",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "238",
+        "source" : "225",
+        "target" : "220",
+        "shared_name" : "FR NPRM (Printed at start of notice) Agency",
+        "name" : "FR NPRM (Printed at start of notice) Agency",
+        "interaction" : "Printed at start of notice",
+        "SUID" : 238,
+        "shared_interaction" : "Printed at start of notice",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "237",
+        "source" : "223",
+        "target" : "236",
+        "shared_name" : "FR Final Rule (Regulations.gov) Regulation Docket",
+        "name" : "FR Final Rule (Regulations.gov) Regulation Docket",
+        "interaction" : "Regulations.gov",
+        "SUID" : 237,
+        "shared_interaction" : "Regulations.gov",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "235",
+        "source" : "223",
+        "target" : "225",
+        "shared_name" : "FR Final Rule (Supplementary Information section) FR NPRM",
+        "name" : "FR Final Rule (Supplementary Information section) FR NPRM",
+        "interaction" : "Supplementary Information section",
+        "SUID" : 235,
+        "shared_interaction" : "Supplementary Information section",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "234",
+        "source" : "223",
+        "target" : "220",
+        "shared_name" : "FR Final Rule (Printed at start of notice) Agency",
+        "name" : "FR Final Rule (Printed at start of notice) Agency",
+        "interaction" : "Printed at start of notice",
+        "SUID" : 234,
+        "shared_interaction" : "Printed at start of notice",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "233",
+        "source" : "221",
+        "target" : "232",
+        "shared_name" : "C.F.R. Section (Authority citation) U.S.C. Section",
+        "name" : "C.F.R. Section (Authority citation) U.S.C. Section",
+        "interaction" : "Authority citation",
+        "SUID" : 233,
+        "shared_interaction" : "Authority citation",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "231",
+        "source" : "221",
+        "target" : "223",
+        "shared_name" : "C.F.R. Section (Source citation) FR Final Rule",
+        "name" : "C.F.R. Section (Source citation) FR Final Rule",
+        "interaction" : "Source citation",
+        "SUID" : 231,
+        "shared_interaction" : "Source citation",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "230",
+        "source" : "221",
+        "target" : "220",
+        "shared_name" : "C.F.R. Section (C.F.R. Chapter) Agency",
+        "name" : "C.F.R. Section (C.F.R. Chapter) Agency",
+        "interaction" : "C.F.R. Chapter",
+        "SUID" : 230,
+        "shared_interaction" : "C.F.R. Chapter",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "226",
+        "source" : "220",
+        "target" : "225",
+        "shared_name" : "Agency (FR Index) FR NPRM",
+        "name" : "Agency (FR Index) FR NPRM",
+        "interaction" : "FR Index",
+        "SUID" : 226,
+        "shared_interaction" : "FR Index",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "224",
+        "source" : "220",
+        "target" : "223",
+        "shared_name" : "Agency (FR Index) FR Final Rule",
+        "name" : "Agency (FR Index) FR Final Rule",
+        "interaction" : "FR Index",
+        "SUID" : 224,
+        "shared_interaction" : "FR Index",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "222",
+        "source" : "220",
+        "target" : "221",
+        "shared_name" : "Agency (C.F.R. Table of Contents) C.F.R. Section",
+        "name" : "Agency (C.F.R. Table of Contents) C.F.R. Section",
+        "interaction" : "C.F.R. Table of Contents",
+        "SUID" : 222,
+        "shared_interaction" : "C.F.R. Table of Contents",
+        "selected" : false
+      },
+      "selected" : false
+    }]
